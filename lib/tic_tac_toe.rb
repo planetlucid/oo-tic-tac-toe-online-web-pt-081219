9e6c1b698384e704a43259a22907497b07c3b116
Ryan S <planetlucid@gmail.com>
2019-09-15 23:47:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,87 +1,118 @@
-class TicTacToe
-  def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def move(board, location, current_player = "X")
+  board[location.to_i-1] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, position)
+  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  if valid_move?(board, input)
+    move(board, input, current_player(board))
+  else
+    turn(board)
   end
+  display_board(board)
+end
 
-  WIN_COMBINATIONS = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-  ]
-
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+def turn_count(board)
+  counter = 0
+  board.each do |i|
+    if i == "X" || i == "O"
+      counter += 1
+    end
   end
+  return counter
+end
 
-  def input_to_index(user_input)
-    user_input.to_i - 1
-  end
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
 
-  def move(index, token)
-    @board[index] = token
-  end
+def won?(board)
 
-  def position_taken?(index)
-    @board[index] != " "
-  end
+  WIN_COMBINATIONS.detect do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-  def valid_move?(index)
-    !position_taken?(index) && index.between?(0,8)
-  end
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
-  def turn_count
-    @board.count{|square| square != " " }
-  end
-
-  def current_player
-    turn_count.even? ? "X" : "O"
-  end
-
-  def turn
-    puts "Please enter a number (1-9):"
-    user_input = gets.strip
-    index = input_to_index(user_input)
-    if valid_move?(index)
-      token = current_player
-      move(index, token)
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
     else
-      turn
-    end
-    display_board
-  end
-
-  def won?
-    WIN_COMBINATIONS.any? do |combo|
-      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
-        return combo
-      end
+      false
     end
   end
+end
 
-  def full?
-    @board.all?{|square| square != " " }
+def full?(board)
+  if board.detect {|i| i == " " || i == nil}
+    return false
+  else
+    return true
   end
+end
 
-  def draw?
-    full? && !won?
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
   end
+end
 
-  def over?
-    won? || draw?
+def over?(board)
+  if draw?(board) || won?(board)
+    return true
+  else
+    return false
   end
+end
 
-  def winner
-    if combo = won?
-      @board[combo[0]]
-    end
+def winner(board)
+  if win_combination = won?(board)
+    board[win_combination.first]
   end
+end
 
-  def play
-    turn until over?
-    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+def play(board)
+  until over?(board)
+    current_player(board)
+    turn(board)
   end
-end 
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  else
+    puts "Cats Game!"
+  end
+end
